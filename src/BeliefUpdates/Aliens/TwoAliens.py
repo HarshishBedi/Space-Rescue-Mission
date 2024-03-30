f94dd3529917cd089cb000d9b9b0d00c674656dd
Sai Harsha Kantamaneni <harsha.kantamneni148@gmail.com>
2024-03-30 13:35:04 -0400

    Vectorized two aliens update

--- src/BeliefUpdates/Aliens/TwoAliens.py
+++ src/BeliefUpdates/Aliens/TwoAliens.py
@@ -67,7 +67,7 @@ def get_transition_prob(open_neighbor_cells):
     open_neighbor_counts[open_neighbor_counts == 0] = np.inf
 
     # Calculate the transition probabilities using broadcasting
-    transition_prob = 1 / open_neighbor_counts[:, :, None, None] / open_neighbor_counts[None, None, :, :]
+    transition_prob = 1 / (open_neighbor_counts[:, :, None, None] * open_neighbor_counts[None, None, :, :])
     return transition_prob
 
 
@@ -80,7 +80,7 @@ def update_belief_matrix_for_two_aliens(belief_matrix_for_two_aliens: np.ndarray
     ship_dim = len(ship_layout)
     observation_matrix_for_two_aliens = get_observation_matrix_for_two_aliens(ship_layout, bot_position, k,
                                                                               alien_sensed)
-    print(f'Time taken for getting observation matrix is {time.time()-start}')
+    print(f'Time taken for getting observation matrix is {time.time() - start}')
     updated_belief_matrix_for_two_aliens = np.zeros((ship_dim, ship_dim, ship_dim, ship_dim), float)
     belief_matrix_for_two_aliens = belief_matrix_for_two_aliens * transition_prob
     for i1 in range(ship_dim):
@@ -91,14 +91,126 @@ def update_belief_matrix_for_two_aliens(belief_matrix_for_two_aliens: np.ndarray
                         # Update belief based on the transition probabilities of neighboring cells
                         for neighbor1 in open_neighbor_cells[i1][j1]:
                             for neighbor2 in open_neighbor_cells[i2][j2]:
-                                if neighbor1 != neighbor2:
-                                    updated_belief_matrix_for_two_aliens[i1][j1][i2][j2] += (
-                                            belief_matrix_for_two_aliens[neighbor1[0]][neighbor1[1]][neighbor2[0]][
-                                                neighbor2[1]] *
-                                            observation_matrix_for_two_aliens[i1][j1][i2][j2])
-    updated_belief_matrix_for_two_aliens = updated_belief_matrix_for_two_aliens * observation_matrix_for_two_aliens
+                                # if neighbor1 != neighbor2:
+                                updated_belief_matrix_for_two_aliens[i1][j1][i2][j2] += (
+                                    belief_matrix_for_two_aliens[neighbor1[0]][neighbor1[1]][neighbor2[0]][
+                                        neighbor2[1]]) * observation_matrix_for_two_aliens[i1][j1][i2][j2]
+    # updated_belief_matrix_for_two_aliens = updated_belief_matrix_for_two_aliens * observation_matrix_for_two_aliens
     # Normalize the updated belief matrix for two aliens
-    total_probability = updated_belief_matrix_for_two_aliens.sum()
-    updated_belief_matrix_for_two_aliens = updated_belief_matrix_for_two_aliens / total_probability
+    # total_probability = updated_belief_matrix_for_two_aliens.sum()
+    # updated_belief_matrix_for_two_aliens = updated_belief_matrix_for_two_aliens / total_probability
     print(f"Time taken for updating belief: {time.time() - start}")
     return updated_belief_matrix_for_two_aliens
+
+
+def update_belief_matrix_for_two_aliens_vectorized(belief_matrix_for_two_aliens: np.ndarray,
+                                                   ship_layout: list[list[str]],
+                                                   bot_position: tuple[int, int],
+                                                   k: int, alien_sensed: bool, transition_prob) -> np.ndarray:
+    start = time.time()
+    ship_dim = len(ship_layout)
+    observation_matrix_for_two_aliens = get_observation_matrix_for_two_aliens(ship_layout, bot_position, k,
+                                                                              alien_sensed)
+    # print(f'Time taken for getting observation matrix is {time.time() - start}')
+    belief_matrix_for_two_aliens = belief_matrix_for_two_aliens * transition_prob
+    updated_belief_matrix_for_two_aliens = add_neighbors(belief_matrix_for_two_aliens)
+    updated_belief_matrix_for_two_aliens = updated_belief_matrix_for_two_aliens * observation_matrix_for_two_aliens
+    for i in range(ship_dim):
+        for j in range(ship_dim):
+            if ship_layout[i][j] == 'C':
+                updated_belief_matrix_for_two_aliens[i][j] = 0
+    # Normalize the updated belief matrix for two aliens
+    # total_probability = updated_belief_matrix_for_two_aliens.sum()
+    # updated_belief_matrix_for_two_aliens = updated_belief_matrix_for_two_aliens / total_probability
+    print(f"Time taken for updating belief: {time.time() - start}")
+    return updated_belief_matrix_for_two_aliens
+
+
+def add_neighbors(belief_matrix):
+    ################################
+    ship_dim = len(belief_matrix)
+    updated_belief_matrix1 = np.zeros((ship_dim, ship_dim, ship_dim, ship_dim), float)
+    shifted_belief = np.concatenate((belief_matrix, np.zeros((1, ship_dim, ship_dim, ship_dim), float)), 0)
+    shifted_belief = shifted_belief[1:, :, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, ship_dim, 1), float), shifted_belief), 3)
+    shifted_belief = shifted_belief[:, :, :, :-1]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((belief_matrix, np.zeros((1, ship_dim, ship_dim, ship_dim), float)), 0)
+    shifted_belief = shifted_belief[1:, :, :, :]
+    shifted_belief = np.concatenate((shifted_belief,np.zeros((ship_dim, ship_dim, ship_dim, 1), float)), 3)
+    shifted_belief = shifted_belief[:, :, :, 1:]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((belief_matrix, np.zeros((1, ship_dim, ship_dim, ship_dim), float)), 0)
+    shifted_belief = shifted_belief[1:, :, :, :]
+    shifted_belief = np.concatenate((shifted_belief,np.zeros((ship_dim, ship_dim, 1, ship_dim), float)), 2)
+    shifted_belief = shifted_belief[:, :, 1:, :]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((belief_matrix, np.zeros((1, ship_dim, ship_dim, ship_dim), float)), 0)
+    shifted_belief = shifted_belief[1:, :, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, 1, ship_dim), float),shifted_belief), 2)
+    shifted_belief = shifted_belief[:, :, :-1, :]
+    updated_belief_matrix1 += shifted_belief
+    ##############################################
+    shifted_belief = np.concatenate((np.zeros((1, ship_dim, ship_dim, ship_dim), float),belief_matrix), 0)
+    shifted_belief = shifted_belief[:-1, :, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, ship_dim, 1), float), shifted_belief), 3)
+    shifted_belief = shifted_belief[:, :, :, :-1]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((np.zeros((1, ship_dim, ship_dim, ship_dim), float),belief_matrix), 0)
+    shifted_belief = shifted_belief[:-1, :, :, :]
+    shifted_belief = np.concatenate((shifted_belief,np.zeros((ship_dim, ship_dim, ship_dim, 1), float)), 3)
+    shifted_belief = shifted_belief[:, :, :, 1:]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((np.zeros((1, ship_dim, ship_dim, ship_dim), float),belief_matrix), 0)
+    shifted_belief = shifted_belief[:-1, :, :, :]
+    shifted_belief = np.concatenate((shifted_belief,np.zeros((ship_dim, ship_dim, 1, ship_dim), float)), 2)
+    shifted_belief = shifted_belief[:, :, 1:, :]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((np.zeros((1, ship_dim, ship_dim, ship_dim), float),belief_matrix), 0)
+    shifted_belief = shifted_belief[:-1, :, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, 1, ship_dim), float),shifted_belief), 2)
+    shifted_belief = shifted_belief[:, :, :-1, :]
+    updated_belief_matrix1 += shifted_belief
+    #########################################
+    shifted_belief = np.concatenate((np.zeros((ship_dim, 1, ship_dim, ship_dim), float), belief_matrix), 1)
+    shifted_belief = shifted_belief[:, :-1, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, ship_dim, 1), float), shifted_belief), 3)
+    shifted_belief = shifted_belief[:, :, :, :-1]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((np.zeros((ship_dim, 1, ship_dim, ship_dim), float), belief_matrix), 1)
+    shifted_belief = shifted_belief[:, :-1, :, :]
+    shifted_belief = np.concatenate((shifted_belief, np.zeros((ship_dim, ship_dim, ship_dim, 1), float)), 3)
+    shifted_belief = shifted_belief[:, :, :, 1:]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((np.zeros((ship_dim, 1, ship_dim, ship_dim), float), belief_matrix), 1)
+    shifted_belief = shifted_belief[:, :-1, :, :]
+    shifted_belief = np.concatenate((shifted_belief, np.zeros((ship_dim, ship_dim, 1, ship_dim), float)), 2)
+    shifted_belief = shifted_belief[:, :, 1:, :]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((np.zeros((ship_dim, 1, ship_dim, ship_dim), float), belief_matrix), 1)
+    shifted_belief = shifted_belief[:, :-1, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, 1, ship_dim), float), shifted_belief), 2)
+    shifted_belief = shifted_belief[:, :, :-1, :]
+    updated_belief_matrix1 += shifted_belief
+    ################################################
+    shifted_belief = np.concatenate((belief_matrix,np.zeros((ship_dim, 1, ship_dim, ship_dim), float)), 1)
+    shifted_belief = shifted_belief[:, 1:, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, ship_dim, 1), float), shifted_belief), 3)
+    shifted_belief = shifted_belief[:, :, :, :-1]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((belief_matrix,np.zeros((ship_dim, 1, ship_dim, ship_dim), float)), 1)
+    shifted_belief = shifted_belief[:, 1:, :, :]
+    shifted_belief = np.concatenate((shifted_belief, np.zeros((ship_dim, ship_dim, ship_dim, 1), float)), 3)
+    shifted_belief = shifted_belief[:, :, :, 1:]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((belief_matrix,np.zeros((ship_dim, 1, ship_dim, ship_dim), float)), 1)
+    shifted_belief = shifted_belief[:, 1:, :, :]
+    shifted_belief = np.concatenate((shifted_belief, np.zeros((ship_dim, ship_dim, 1, ship_dim), float)), 2)
+    shifted_belief = shifted_belief[:, :, 1:, :]
+    updated_belief_matrix1 += shifted_belief
+    shifted_belief = np.concatenate((belief_matrix,np.zeros((ship_dim, 1, ship_dim, ship_dim), float)), 1)
+    shifted_belief = shifted_belief[:, 1:, :, :]
+    shifted_belief = np.concatenate((np.zeros((ship_dim, ship_dim, 1, ship_dim), float), shifted_belief), 2)
+    shifted_belief = shifted_belief[:, :, :-1, :]
+    updated_belief_matrix1 += shifted_belief
+    return updated_belief_matrix1
